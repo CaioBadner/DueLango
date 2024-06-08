c61efae87e2bc137aa1592a05255456d7b171153
CaioBadner <59920967+CaioBadner@users.noreply.github.com>
2024-06-08 09:59:58 +0300

    Update api.js
    Removed the api-key hehe

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const apiClient = axios.create({
     baseURL: 'https://eme0pvxirc.execute-api.us-east-1.amazonaws.com/Prod', 
     headers: {
         'Content-Type': 'application/json',
-        'x-api-key': 'ZJmpAg3CY17fq9uvTiGR91hVYPv0P16I1pu0cjyo'
+        'x-api-key': '.......................................'
     }
 });
 
